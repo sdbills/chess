f8ffeacba37c210ec40fa8d39a0e761059a4820f
sdbills <>
2025-02-02 10:18:35 -0700

    Added getKingPosition method

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -29,6 +29,21 @@ public class ChessBoard {
         }
     }
 
+    public ChessPosition getKingPosition(ChessGame.TeamColor team) {
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                var position = new ChessPosition(i, j);
+                if (!isEmptyPosition(position)) {
+                    var piece = getPiece(position);
+                    if (piece.getPieceType() == KING && piece.getTeamColor() == team) {
+                        return position;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
     /**
      * Adds a chess piece to the chessboard
      *
