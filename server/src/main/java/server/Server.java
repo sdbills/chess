eda9418483f9dd243fed17af64a222bbb9c554cb
sdbills <>
2025-02-22 22:38:04 -0700

    Initial register handling

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,24 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import service.GameService;
+import service.UserService;
 import spark.*;
 
 public class Server {
+    private final UserService userService;
+    private final GameService gameService;
+
+    public Server() {
+        UserDAO userDAO = new MemoryUserDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        this.userService = new UserService(userDAO, authDAO);
+        this.gameService = new GameService(gameDAO, authDAO);
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -14,6 +30,8 @@ public class Server {
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
+        Spark.post("/user", this::register);
+
         Spark.awaitInitialization();
         return Spark.port();
     }
@@ -21,5 +39,11 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object register(Request req, Response res) throws DataAccessException {
+        var user = new Gson().fromJson(req.body(), UserData.class);
+        AuthData result = userService.register(user);
+        return new Gson().toJson(result);
     }
 }
