f080b414ebc9589a9c6ca8b849ef2859e5604cc1
sdbills <>
2025-02-22 18:11:28 -0700

    Initial DAO interfaces

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,13 @@
+package dataaccess;
+
+import model.AuthData;
+
+public interface AuthDAO {
+    AuthData createAuth(AuthData auth) throws DataAccessException;
+
+    AuthData getAuth(String authToken) throws DataAccessException;
+
+    void deleteAuth(AuthData auth) throws DataAccessException;
+
+    void clear() throws DataAccessException;
+}
---
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -0,0 +1,21 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.util.Collection;
+
+public interface GameDAO {
+    GameData createGame(GameData game) throws DataAccessException;
+
+    GameData getGame(String gameName) throws DataAccessException;
+
+    GameData getGame(int gameID) throws DataAccessException;
+
+    void updateGame(GameData game) throws DataAccessException;
+
+    Collection<GameData> listGames() throws DataAccessException;
+
+    void deleteGame(GameData game) throws DataAccessException;
+
+    void clear() throws DataAccessException;
+}
