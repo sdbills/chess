955860558b74aae2086c70978c357f9da06ed014
sdbills <>
2025-02-23 15:29:01 -0700

    Fixed listGames response

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -8,6 +8,7 @@ import dataaccess.ResponseException;
 import model.GameData;
 import request.JoinRequest;
 import response.CreateResponse;
+import response.ListResponse;
 
 import java.util.Collection;
 
@@ -36,13 +37,13 @@ public class GameService extends Service {
         }
     }
 
-    public Collection<GameData> listGames(String authToken) throws DataAccessException, ResponseException {
+    public ListResponse listGames(String authToken) throws DataAccessException, ResponseException {
         authenticate(authToken);
 
         var games = new java.util.ArrayList<>(gameDAO.listGames());
         games.replaceAll(game -> new GameData(game.gameID(),
                 game.whiteUsername(), game.blackUsername(), game.gameName(), null));
-        return games;
+        return new ListResponse(games);
     }
 
     public void joinGame(JoinRequest req, String authToken) throws DataAccessException, ResponseException {
---
+++ shared/src/main/java/response/ListResponse.java
@@ -0,0 +1,8 @@
+package response;
+
+import model.GameData;
+
+import java.util.Collection;
+
+public record ListResponse(Collection<GameData> games) {
+}
