163d8d73376cf866f2796be2a6348b09bb29bf0d
sdbills <>
2025-02-22 18:19:29 -0700

    Initial UserDAO implementation

---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,24 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO{
+    private final HashMap<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        users.put(user.username(),user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+    }
+}
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -3,7 +3,7 @@ package dataaccess;
 import model.UserData;
 
 public interface UserDAO {
-    UserData createUser(UserData user) throws DataAccessException;
+    void createUser(UserData user) throws DataAccessException;
 
     UserData getUser(String username) throws DataAccessException;
 
