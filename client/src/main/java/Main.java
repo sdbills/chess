1fbd6140fde3e31a9953507c7c2dca0ded784c1c
sdbills <>
2025-03-24 21:12:04 -0600

    Initialize repl

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,16 @@
 import chess.*;
+import ui.Repl;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Client: " + piece);
+
+        var port = 8080;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        new Repl(port).run();
     }
 }
