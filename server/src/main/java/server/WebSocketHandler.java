3f9c0f0ca071434e08d8403f3cc3b9232ed27816
sdbills <>
2025-04-09 23:31:20 -0600

    Add Basic structure

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -5,6 +5,7 @@ import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import exception.ResponseException;
 import model.AuthData;
+import org.eclipse.jetty.server.Authentication;
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import websocket.commands.UserGameCommand;
@@ -21,5 +22,25 @@ public class WebSocketHandler {
 
         String user = Server.authenticate(command.getAuthToken()).username();
         connections.add(session, command.getGameID());
+
+        switch (command.getCommandType()) {
+            case CONNECT -> connect(session, user, command);
+            case MAKE_MOVE -> makeMove(session, user, command);
+            case LEAVE -> leaveGame(session, user, command);
+            case RESIGN -> resign(session, user, command);
+        }
     }
+
+    private void connect(Session session, String user, UserGameCommand command) {
+    }
+
+    private void makeMove(Session session, String user, UserGameCommand command) {
+    }
+
+    private void leaveGame(Session session, String user, UserGameCommand command) {
+    }
+
+    private void resign(Session session, String user, UserGameCommand command) {
+    }
+
 }
