d5137cc6a3221ba860ecce34dff181bc3eb2d8f5
sdbills <>
2025-04-09 23:40:49 -0600

    Added ServerMessage subclasses

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,15 @@
+package websocket.messages;
+
+public class ErrorMessage extends ServerMessage{
+
+    String message;
+
+    public ErrorMessage(String message) {
+        super(ServerMessageType.ERROR);
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
